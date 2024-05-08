378903b9826ea27c75674db735729b9da4a5993e
ValeMat78 <valerio.evarchi@mat.tn.it>
2024-05-08 13:58:05 +0200

    aggiunta di aggiunta tag e ricerca singola persona

--- demo-rubrica/src/main/java/com/corsoaltaformazione/demorubrica/controllers/PersoneController.java
+++ demo-rubrica/src/main/java/com/corsoaltaformazione/demorubrica/controllers/PersoneController.java
@@ -1,47 +1,64 @@
+// Package declaration for the controller classes
 package com.corsoaltaformazione.demorubrica.controllers;
 
+// Import statements for necessary classes
 import java.util.List;
 
+import com.corsoaltaformazione.demorubrica.models.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.corsoaltaformazione.demorubrica.models.Persona;
 import com.corsoaltaformazione.demorubrica.services.RubricaService;
 
+// Request mapping for the "/rubrica" endpoint and defining a RestController
 @RequestMapping("/rubrica")
 @RestController
 public class PersoneController {
+
+    // Autowiring the RubricaService to access its methods
     @Autowired
     RubricaService rubricaService;
 
+    // GET mapping to retrieve all personas
     @GetMapping("")
     public @ResponseBody List<Persona> getAll(){
+        // Calling the findAll() method of the RubricaService to retrieve all personas
         return rubricaService.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Persona getById(String id){
-        return null;
+    // GET mapping to retrieve a specific persona by id
+    @GetMapping("/{email}")
+    public Persona getById(@PathVariable String email){
+        // Calling the getPersona() method of the RubricaService to retrieve a persona by email
+        return rubricaService.getPersona(email);
     }
 
+    // POST mapping to save a new persona
     @PostMapping("")
     public @ResponseBody Persona save(@RequestBody Persona persona){
+        // Calling the addPersona() method of the RubricaService to save a new persona
         rubricaService.addPersona(persona);
         return persona;
     }
 
+    // DELETE mapping to delete a persona by email
     @DeleteMapping("/{email}")
     public ResponseEntity<Boolean> delete(@PathVariable String email){
+        // Calling the deletePersona() method of the RubricaService to delete a persona by email
         rubricaService.deletePersona(email);
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
+
+    // PUT mapping to update a persona's tags
+    @PutMapping("/{email}")
+    public ResponseEntity<Boolean> update(@RequestBody TagRequest sag){
+        // Retrieving the persona by email using the getPersona() method of the RubricaService
+        Persona persona = rubricaService.getPersona(sag.getEmail());
+        // Adding tags to the persona using the addTag() method of the RubricaService
+        rubricaService.addTag(sag.getEmail(), persona, sag.getTags());
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
 }
--- demo-rubrica/src/main/java/com/corsoaltaformazione/demorubrica/services/RubricaService.java
+++ demo-rubrica/src/main/java/com/corsoaltaformazione/demorubrica/services/RubricaService.java
@@ -1,26 +1,48 @@
-package com.corsoaltaformazione.demorubrica.services;
+package com.corsoaltaformazione.demorubrica.services; // Declares the package for the RubricaService class.
 
-import java.util.List;
+import java.util.ArrayList; // Imports the ArrayList class from the java.util package.
+import java.util.List; // Imports the List interface from the java.util package.
+import java.util.StringJoiner; // Imports the StringJoiner class from the java.util package.
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.corsoaltaformazione.demorubrica.models.Tag; // Imports the Tag class from the models package.
+import org.springframework.beans.factory.annotation.Autowired; // Imports the Autowired annotation from the Spring framework.
+import org.springframework.stereotype.Service; // Imports the Service annotation from the Spring framework.
 
-import com.corsoaltaformazione.demorubrica.models.Persona;
-import com.corsoaltaformazione.demorubrica.repository.Db;
+import com.corsoaltaformazione.demorubrica.models.Persona; // Imports the Persona class from the models package.
+import com.corsoaltaformazione.demorubrica.repository.Db; // Imports the Db class from the repository package.
 
-@Service
-public class RubricaService {
-    @Autowired
-    private Db db;
+@Service // Indicates that the RubricaService class is a service component.
 
-    public void addPersona(Persona persona){
-        db.getDb().put(persona.getEmail(), persona);
+public class RubricaService { // Declares the RubricaService class.
+
+    @Autowired // Marks the db field for automatic dependency injection.
+    private Db db; // Declares a private field db of type Db.
+
+    public void addPersona(Persona persona){ // Declares a method to add a person to the address book.
+        db.getDb().put(persona.getEmail(), persona); // Adds the person to the database.
     }
-    public void deletePersona(String email){
-        db.getDb().remove(email);
+
+    public void addTag(String email,Persona persona, List<Tag> tags){ // Declares a method to add tags to a person.
+        if(persona.getTags() == null){ // Checks if the person has no tags.
+            persona.setTags(new ArrayList<>()); // Creates a new list for tags if none exists.
+            persona.getTags().addAll(tags); // Adds the new tags to the person.
+        }
+        else { // If the person has existing tags.
+            persona.getTags().addAll(tags); // Adds the new tags to the person.
+        }
+        db.getDb().put(email, persona); // Updates the person in the database.
     }
-    public List<Persona> findAll(){
-        return db.getDb().values().stream().toList();
+
+    public Persona getPersona(String email){ // Declares a method to get a person by email.
+        return db.getDb().get(email); // Retrieves the person from the database by email.
+    }
+
+    public void deletePersona(String email){ // Declares a method to delete a person by email.
+        db.getDb().remove(email); // Removes the person from the database.
+    }
+
+    public List<Persona> findAll(){ // Declares a method to find all persons in the address book.
+        return db.getDb().values().stream().toList(); // Returns a list of all persons in the database.
     }
 
 }
